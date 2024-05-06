4c0be6ed3cbc2327a555652db2b1240c9351720e
Akash Patel <aksp1790@gmail.com>
2024-05-06 11:50:13 +0530

    initial commit

---
+++ src/test/java/org/example/ecommeurekaserver/EcommEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.ecommeurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
